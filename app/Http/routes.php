21405cc00f64d292d8748664964c75ec221941a3
Alexandre <alexmart4@gmail.com>
2015-05-14 18:20:21 +0200

    Afegit el layout.blade.php per poder veure la plantilla bootstrap

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
+Route::get('/', function(){
+    return View::make('layout');
+});
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
