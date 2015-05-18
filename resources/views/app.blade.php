7629e3837386d8ded2aa8e617d2e3938fd9468c5
Alexandre <alexmart4@gmail.com>
2015-05-18 16:51:03 +0200

    modificacions menors per millorar el llenguatge de la pàgina

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -28,7 +28,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Ebre-escool Laravel</a>
+            <a class="navbar-brand" href="#">Ebre-escool</a>
         </div>
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -38,13 +38,13 @@
 
             <ul class="nav navbar-nav navbar-right">
                 @if (Auth::guest())
-                    <li><a href="/auth/login">Login</a></li>
-                    <li><a href="/auth/register">Register</a></li>
+                    <li><a href="/auth/login">Entrar</a></li>
+                    <li><a href="/auth/register">Registrar-se</a></li>
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
                         <ul class="dropdown-menu" role="menu">
-                            <li><a href="/auth/logout">Logout</a></li>
+                            <li><a href="/auth/logout">Sortir</a></li>
                         </ul>
                     </li>
                 @endif
