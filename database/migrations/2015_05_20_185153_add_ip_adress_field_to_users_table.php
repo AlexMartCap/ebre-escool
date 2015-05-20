b7ac1cf15aae2a088f86560c96b8d38b28e9378f
Alexandre <alexmart4@gmail.com>
2015-05-20 20:57:00 +0200

    Migracions de la taula users

---
+++ database/migrations/2015_05_20_185153_add_ip_adress_field_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIpAdressFieldToUsersTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function(Blueprint $table)
+        {
+            $table->integer('ip_address');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('users', function(Blueprint $table)
+        {
+            $table->dropColumn(
+                'ip_address'
+            );
+        });
+
+    }
+
+}
---
+++ database/migrations/2015_05_20_185505_add_mainOrganizationaUnitId_field_to_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddMainOrganizationaUnitIdFieldToUsersTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function(Blueprint $table)
+        {
+            $table->integer('mainOrganizationaUnitId');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('users', function(Blueprint $table)
+        {
+            $table->dropColumn(
+                'mainOrganizationaUnitId'
+            );
+        });
+
+    }
+
+}
